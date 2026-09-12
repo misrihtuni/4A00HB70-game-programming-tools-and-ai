f55307dd1d3700ec098626129a1d945582af84f3
Miska Rihu <229709985+misrihtuni@users.noreply.github.com>
2026-09-12 23:01:37 +0300

    Tests: Add tests for removing items from the LinkedList

--- Ships.Tests/LinkedListTests.cs
+++ Ships.Tests/LinkedListTests.cs
@@ -14,4 +14,58 @@ public class LinkedListTests
 		Assert.Equal(3, list.Count);
 		Assert.Equal("1,6,-1", list.TestGetAsString());
 	}
+
+
+	#region Removing Items
+
+	[Fact]
+	public void TestRemoveSingle()
+	{
+		GA.Collections.LinkedList<int> list = [1];
+		Assert.True(list.Remove(1));
+		Assert.Empty(list);
+		Assert.Equal(0, list.Count);
+		Assert.Equal("", list.TestGetAsString());
+	}
+
+	[Fact]
+	public void TestRemoveFirst()
+	{
+		GA.Collections.LinkedList<int> list = [1, 0, 3, 0, 5];
+		Assert.True(list.Remove(1));
+		Assert.Equal(4, list.Count);
+		Assert.Equal("0,3,0,5", list.TestGetAsString());
+	}
+
+	[Fact]
+	public void TestRemoveFromMiddle()
+	{
+		GA.Collections.LinkedList<int> list = [1, 0, 3, 0, 5];
+		Assert.True(list.Remove(3));
+		Assert.Equal(4, list.Count);
+		Assert.Equal("1,0,0,5", list.TestGetAsString());
+	}
+
+	[Fact]
+	public void TestRemoveDuplicate()
+	{
+		GA.Collections.LinkedList<int> list = [1, 0, 3, 0, 5];
+		Assert.True(list.Remove(0));
+		Assert.Equal(4, list.Count);
+		Assert.Equal("1,3,0,5", list.TestGetAsString());
+		Assert.True(list.Remove(0));
+		Assert.Equal(3, list.Count);
+		Assert.Equal("1,3,5", list.TestGetAsString());
+	}
+
+	[Fact]
+	public void TestRemoveLast()
+	{
+		GA.Collections.LinkedList<int> list = [1, 0, 3, 0, 5];
+		Assert.True(list.Remove(5));
+		Assert.Equal(4, list.Count);
+		Assert.Equal("1,0,3,0", list.TestGetAsString());
+	}
+
+	#endregion Removing Items
 }
