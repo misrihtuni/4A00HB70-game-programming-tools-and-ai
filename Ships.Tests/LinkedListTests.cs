a079a34fa0e4d028c6581bad07dc79b51302a7d5
Miska Rihu <229709985+misrihtuni@users.noreply.github.com>
2026-09-13 21:05:19 +0300

    Tests: Add tests for LinkedList.Contains

--- Ships.Tests/LinkedListTests.cs
+++ Ships.Tests/LinkedListTests.cs
@@ -15,6 +15,117 @@ public class LinkedListTests
 		Assert.Equal("1,6,-1", list.TestGetAsString());
 	}
 
+	#region Contains
+
+	/// <summary>
+	/// Tests the Contains method when the list is empty.
+	/// </summary>
+	[Fact]
+	public void Contains_WhenEmpty_ReturnsFalse()
+	{
+		GA.Collections.LinkedList<int> list = new GA.Collections.LinkedList<int>();
+
+		bool result = list.Contains(1);
+
+		Assert.False(result);
+	}
+
+	/// <summary>
+	/// Tests the Contains method when the existing item is the only item in
+	/// the list.
+	/// </summary>
+	[Fact]
+	public void Contains_WhenSingleItemAndDesiredExists_RetursnTrue()
+	{
+		GA.Collections.LinkedList<int> list = new GA.Collections.LinkedList<int>();
+		list.Add(1);
+
+		bool result = list.Contains(1);
+
+		Assert.True(result);
+	}
+
+	/// <summary>
+	/// Tests the Contains method when there is only one item in the list and
+	/// it is not the one searched for.
+	/// </summary>
+	[Fact]
+	public void Contains_WhenSingleItemAndDesiredNotExists_ReturnsFalse()
+	{
+		GA.Collections.LinkedList<int> list = new GA.Collections.LinkedList<int>();
+		list.Add(1);
+
+		bool result = list.Contains(0);
+
+		Assert.False(result);
+	}
+
+	/// <summary>
+	/// Tests the Contains method when the desired item is the first one in the
+	/// list.
+	/// </summary>
+	[Fact]
+	public void Contains_WhenDesiredItemIsFirst_ReturnsTrue()
+	{
+		GA.Collections.LinkedList<int> list = new GA.Collections.LinkedList<int>();
+		list.Add(1);
+		list.Add(2);
+		list.Add(3);
+
+		bool result = list.Contains(1);
+
+		Assert.True(result);
+	}
+
+	/// <summary>
+	/// Tests the Contains method when the desired item is in the middle of the
+	/// list.
+	/// </summary>
+	[Fact]
+	public void Contains_WhenDesiredItemIsMiddle_ReturnsTrue()
+	{
+		GA.Collections.LinkedList<int> list = new GA.Collections.LinkedList<int>();
+		list.Add(1);
+		list.Add(2);
+		list.Add(3);
+
+		bool result = list.Contains(2);
+
+		Assert.True(result);
+	}
+
+	/// <summary>
+	/// Tests the contains method when the desired item is the last item in the
+	/// list.
+	/// </summary>
+	[Fact]
+	public void Contains_WhenDesiredItemIsLast_ReturnsTrue()
+	{
+		GA.Collections.LinkedList<int> list = new GA.Collections.LinkedList<int>();
+		list.Add(1);
+		list.Add(2);
+		list.Add(3);
+
+		bool result = list.Contains(3);
+
+		Assert.True(result);
+	}
+
+	[Fact]
+	public void Contains_WhenMultipleItemsAndDesiredNotExists_ReturnsFalse()
+	{
+		GA.Collections.LinkedList<int> list = new GA.Collections.LinkedList<int>();
+		list.Add(1);
+		list.Add(2);
+		list.Add(3);
+
+		bool result = list.Contains(0);
+
+		Assert.False(result);
+	}
+
+	#endregion Contains
+
 
 	#region Removing Items
 
